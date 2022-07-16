a23bd2467bd3d6d7b81f3e46b5f3ff3878e5cc7f
mihailov_er1999@mail.ru <Mer19990213>
2022-07-16 20:48:27 +0300

    Maksim Lesson 3

---
+++ Lesson_03/lesson_03_list.py
@@ -0,0 +1,38 @@
+# Расширение списка другим списком
+list_for_extended = [1, 2, 3, 4]
+list_to_extended = [5, 6, 7, 8]
+extended_list = list_for_extended
+extended_list.extend(list_to_extended)
+# print(extended_list)
+# внедрение элемента под некоторым номером
+
+element_to_insert = 10
+extended_list.insert(5, element_to_insert)
+# print(extended_list)
+
+# удаление первого элемента, имеющего указанное значение
+list_to_destract = ['life, my life ', '45', 45, 'zusamen', 'extended man']
+list_to_destract.remove(45)
+# print(list_to_destract)
+
+# передает удаленный элемент в переменную
+
+pop_element = list_to_destract.pop(2)
+# print(pop_element)
+# print(list_to_destract)
+
+# функция index
+
+# my_element = '451'
+# if my_element in list_to_destract:
+#     index_my_element = list_to_destract.index(my_element)
+#     print(f'Element {my_element} has index: {index_my_element}')
+# else:
+#     print(f'{my_element} not in list')
+# Число элементов определенного значения в списке
+list_of_my_class = ['Alex', 'Ann', 'Nick', 'Guntter', 'Ferdoves', 'Galptilfart', 'Djafar', 'Alladin', 'Ann', 'Ann']
+count_of_Ann = list_of_my_class.count('Ann')
+print(count_of_Ann)
+
+
+
---
+++ Lesson_03/lesson_03_while.py
@@ -0,0 +1,28 @@
+# Цикл while
+i = -5
+number_for_example = 0
+# while i < 0:
+#     i += 1 # i=i+1
+#     number += 1
+#     print(f'Какой хороший день, какой хороший пень, какой хороший я и песенка моя!!!____ Итерация номер {number}, число i = {i}')
+
+# С условием выхода
+
+# while True:
+#     my_number = int(input('Введите число: '))
+#     my_number += 25
+#     if my_number <= 78:
+#         continue
+#     elif my_number >= 78 and my_number < 95:
+#         continue
+#     else:
+#         print(my_number)
+#         break
+
+while number_for_example == 0:
+    for val in range(35):
+        print(f'Iteration number, {val +1}, {2**val}')
+        # number = 0
+    else:
+        number_for_example = 1
+
