f06bd02fd13dc2a68264a6f61b371906204cae1f
maximdoroginskiy <ma.doroginskiy@gmail.com>
2022-07-29 19:26:21 +0200

    задание 1 сделано

--- Lesson_03/Home_work_lesson_03/01_days_in_month.py
+++ Lesson_03/Home_work_lesson_03/01_days_in_month.py
@@ -7,9 +7,14 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input('Введите, пожалуйста, номер месяца: ')
+
+user_input = int(input('Введите, пожалуйста, номер месяца: '))
+if user_input == 1 or user_input == 3 or user_input == 5 or user_input == 7 or user_input == 8 or user_input == 10 or user_input == 12:
+    print(f'В {user_input}-ом месяце 31 день')
+elif user_input == 4 or user_input == 6 or user_input == 9 or user_input == 11:
+    print(f'В {user_input}-ом месяце 30 дней')
+elif user_input > 12 or user_input == 0 or user_input <= 0:
+    print(f'В {user_input}-ом месяце пока что, к сожалению, ноль дней')
+else:
+    print(f'Во {user_input}-ом месяце 28 дней')
 print('Для выхода введите -13')
-
-
-
-
