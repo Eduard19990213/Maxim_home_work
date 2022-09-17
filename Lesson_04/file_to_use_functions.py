521a2abb78c6cb956f9ca70f5337142842fdf5a1
eduard <mihailov_er1999@mail.ru>
2022-09-17 19:13:10 +0300

    Lesson 5 Begin

--- Lesson_04/file_to_use_functions.py
+++ Lesson_04/file_to_use_functions.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from Functions import fibbonachi_numbers, trancformer_list_to_set
 
 example_1 = fibbonachi_numbers(15)
-# pprint(example_1)
+pprint(example_1)
 
 list_of_names = ['Ann', 'Sergo', 'Nick', 'Sedrick', 'Sam', 'sam', 'Ann',
                  'Ann', 'Sergo', 'Nick', 'Sedrick', 'Sam', 'sam', 5, '5', 5, 50, 55,
---
+++ Lesson_05/draw_function.py
@@ -0,0 +1,9 @@
+import simple_draw as sd
+
+first_point = sd.get_point(250, 250)
+
+
+
+
+
+sd.pause()
