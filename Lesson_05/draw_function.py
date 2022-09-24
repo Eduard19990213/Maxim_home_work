dbc5f696802998f9986b05bf5d148060da097baa
maximdoroginskiy <ma.doroginskiy@gmail.com>
2022-09-24 18:02:50 +0200

    задание 5

--- Lesson_05/draw_function.py
+++ Lesson_05/draw_function.py
@@ -2,30 +2,32 @@ import math
 
 import simple_draw as sd
 
-sd.background_color = (127, 0, 127)
+sd.background_color = (255, 255, 255)
 sd.resolution = (1200, 900)
 sd.caption = 'Наше рабочее пространство'
-first_point = sd.get_point(250, 250)
+first_point = sd.get_point(500, 500)
 
 
-# vector_1 = sd.get_vector(first_point, 0, 150, 5)
+#vector_1 = sd.get_vector(first_point, 0, 150, 5)
 # vector_1.draw()
 # vector_2 = sd.get_vector(start_point=vector_1.end_point, width=5, length=200, angle=45)
 # vector_2.draw()
 
 def benzol(start_point, length=200, angle=90):
-    start_point = sd.get_point(start_point.x + length, start_point.y + length / 2)
-    x_circle = start_point.x + length * math.sin(math.pi/3) - length*1.75
-    y_circle = start_point.y + length * math.cos(math.pi / 3)
-    circle = sd.circle(center_position=sd.get_point(x_circle, y_circle), radius=length * 0.68, color=sd.COLOR_YELLOW, width=5)
+    # start_point = sd.get_point(start_point.x + length, start_point.y + length / 2)
+    # x_circle = start_point.x + length * math.sin(math.pi / 3) - length * 1.75
+    # y_circle = start_point.y + length * math.cos(math.pi / 3)
+    # circle = sd.circle(center_position=sd.get_point(x_circle, y_circle), radius=length * 0.68, color=sd.COLOR_BLACK,
+    #                    width=10)
 
     for number in range(6):
-        vector = sd.get_vector(start_point=start_point, width=5, length=length, angle=angle + 60 * number)
+        vector = sd.get_vector(start_point=start_point, width=10, length=length, angle=angle + 60 * number)
         vector.draw()
         start_point = vector.end_point
 
 
 benzol(start_point=first_point)
+
 
 # benzol(start_point=sd.get_point(280,350))
 
@@ -35,12 +37,15 @@ def grid(resolution=sd.resolution, step=50):
     x_grid = step
     y_grid = step
     while x_grid < x:
-        x_line = sd.line(start_point=sd.get_point(x_grid, 0), end_point=sd.get_point(x_grid, y), color=sd.COLOR_WHITE, width=1)
+        x_line = sd.line(start_point=sd.get_point(x_grid, 0), end_point=sd.get_point(x_grid, y), color=sd.COLOR_BLACK,
+                         width=1)
         x_grid += step
 
     while y_grid < y:
-        y_line = sd.line(start_point=sd.get_point(0, y_grid), end_point=sd.get_point(x, y_grid), color=sd.COLOR_WHITE, width=1)
+        y_line = sd.line(start_point=sd.get_point(0, y_grid), end_point=sd.get_point(x, y_grid), color=sd.COLOR_BLACK,
+                         width=1)
         y_grid += step
+
 
 grid()
 
--- Lesson_05/hm_05.py
+++ Lesson_05/hm_05.py
@@ -1,3 +1,47 @@
 # Написать функции по отрисовке правильного многоугольника с n сторонами с любым начальным углом поворота
 # Угол в правильном n-угольнике составляет 180*(n-2)/n в градусах, сделать все через линии,
 # чтобы цвета можно было менять, как в нашей сетке
+
+
+import simple_draw as sd
+
+sd.background_color = (255, 255, 255)
+sd.resolution = (1200, 900)
+sd.caption = 'Моё рабочее пространство'
+first_point = sd.get_point(500, 500)
+n = int(input())
+x = (180*(n-2)/n - 90)*2
+#second_point = sd.get_point(500, 700)
+# v1 = sd.get_vector(start_point=first_point, angle=0, length=150, width=5, )
+# v1.draw()
+# v2 = sd.get_vector(second_point, 90, 200, 10)
+# v2.draw()
+
+def Mnogougolnik(start_point, length= 100, angle= 0):
+    for number in range(n):
+        vector = sd.get_vector(start_point=start_point, width=10, length=length, angle=angle + x * number)
+        vector.draw()
+        start_point = vector.end_point
+
+Mnogougolnik(start_point=first_point)
+
+
+
+def grid(resolution=sd.resolution, step=50):
+    x = resolution[0]
+    y = resolution[1]
+    x_grid = step
+    y_grid = step
+    while x_grid < x:
+        xl = sd.line(start_point=sd.get_point(x_grid, 0), end_point=sd.get_point(x_grid, y), color=sd.COLOR_BLACK,
+                     width=1)
+        x_grid += step
+
+    while y_grid < y:
+        yl = sd.line(start_point=sd.get_point(0, y_grid), end_point=sd.get_point(x, y_grid), color=sd.COLOR_BLACK,
+                     width=1)
+        y_grid += step
+
+grid()
+
+sd.pause()
